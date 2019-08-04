b7a323fbb55e08a41da4b70c7fbad314b1ce5bf0
1 <1@2.3>
2019-08-04 21:34:46 +0300

    minimax engine

---
+++ chess2.h
@@ -0,0 +1,15 @@
+/*
+ * chess2.h
+ *
+ *  Created on: 4 рту. 2019 у.
+ *      Author: URA
+ */
+
+#ifndef CHESS2_H_
+#define CHESS2_H_
+
+
+extern void chess_init(void);
+
+
+#endif /* CHESS2_H_ */
---
+++ common.h
@@ -0,0 +1,18 @@
+/*
+ * common.h
+ *
+ *  Created on: 4 рту. 2019 у.
+ *      Author: URA
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#define	uint8_t unsigned char
+#define	uint16_t unsigned short
+#define	uint32_t unsigned int
+#define	int8_t signed char
+#define	int16_t signed short
+#define	int32_t signed int
+
+#endif /* COMMON_H_ */
